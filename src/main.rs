77bcbafe84414ec9a385604e20e5de69f509f4fd
lifeavg <44943333+lifeavg@users.noreply.github.com>
2026-03-15 00:02:20 +0300

    add logs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::time::{SystemTime, UNIX_EPOCH};
 use redis::{AsyncCommands, Client};
+use log;
+use env_logger::Env;
 
 const HOST: &str = "localhost:3000";
 
@@ -32,22 +34,38 @@ select link, \"end\" from links where token=$1 and $2 between \"start\" and \"en
 
 #[tokio::main]
 async fn main() {
+    env_logger::Builder::from_env(Env::default().filter_or("SHORTCUT_LOG", "info")).init();
     let pool = PgPoolOptions::new()
         .max_connections(2)
-        .connect("postgres://postgres:postgres@localhost/postgres").await.unwrap();
-    sqlx::query(INIT_DB).execute(&pool).await.unwrap();
+        .connect("postgres://postgres:postgres@localhost/postgres").await.unwrap_or_else(|e| {
+            log::error!("Failed to connect to database: {}", e);
+            panic!("Exit");
+        });
+    sqlx::query(INIT_DB).execute(&pool).await.unwrap_or_else(|e| {
+        log::error!("Failed to initialize database: {}", e);
+        panic!("Exit");
+    });
 
-    let redis_client = redis::Client::open("redis://localhost/").unwrap();
+    let redis_client = redis::Client::open("redis://localhost/").unwrap_or_else(|e| {
+        log::error!("Failed to connect to Redis: {}", e);
+        panic!("Exit");
+    });
 
     let state = AppState{pool: pool, redis: redis_client};
     let app = Router::new()
     .route("/", routing::post(new_shortcut_handler))
     .route("/{token}", routing::get(follow_shortcut_handler))
     .with_state(state);
-    println!("router created");
-    let listener = tokio::net::TcpListener::bind(HOST).await.unwrap();
-    println!("serving http://{}/", HOST);
-    axum::serve(listener, app).await.unwrap();
+    log::info!("router created");
+    let listener = tokio::net::TcpListener::bind(HOST).await.unwrap_or_else(|e| {
+        log::error!("Failed to bind host: {}", e);
+        panic!("Exit");
+    });
+    log::info!("serving http://{}/", HOST);
+    axum::serve(listener, app).await.unwrap_or_else(|e| {
+        log::error!("Failed to start server: {}", e);
+        panic!("Exit");
+    });
 }
 
 fn new_token() -> String {
@@ -102,38 +120,77 @@ impl fmt::Display for NewShortcut {
     }
 }
 
-async fn new_shortcut_handler(State(state): State<AppState>, Json(payload): Json<CreateShortcut>) -> Json<NewShortcut> {
+async fn new_shortcut_handler(State(state): State<AppState>, Json(payload): Json<CreateShortcut>) ->  Result<Json<NewShortcut>, StatusCode> {
     let token = new_token();
-    sqlx::query(INSERT_LINK).persistent(true).bind(&token).bind(&payload.url).bind(payload.start).bind(payload.end).execute(&state.pool).await.unwrap();
+    sqlx::query(INSERT_LINK).persistent(true).bind(&token).bind(&payload.url).bind(payload.start).bind(payload.end).execute(&state.pool).await.map_err(|e| {
+        log::error!("Failed to save shortcut to database token={}, link={} : {}", &token, &payload.url, e);
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
     let resp = NewShortcut {
         create: payload,
         url: format!("http://{}/{}", HOST, &token),
     };
-    Json(resp)
+    log::info!("Created new shortcut token={}, link={}", &token, &resp.create.url);
+    Ok(Json(resp))
 }
 
 async fn follow_shortcut_handler(State(state): State<AppState>, Path(token): Path<String>) -> Result<Redirect, StatusCode> {
-    let mut conn = state.redis.get_multiplexed_async_connection().await.unwrap();
-    let cached_redirect: Result<String, redis::RedisError> =  conn.get(&token).await;
-    if cached_redirect.is_ok() {
-        println!("Responded from cache token={}", &token);
-        return Ok(Redirect::temporary(cached_redirect.ok().unwrap().as_str()))
-    }
-    let now = SystemTime::now();
-    let duration_since_epoch = now
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    let timestamp_secs = duration_since_epoch.as_secs() as i32;
-    let row: (String, i32) = sqlx::query_as(FIND_REDIRECT).persistent(true).bind(&token).bind(timestamp_secs).fetch_one(&state.pool).await.unwrap_or_default();
-    if row.0 == "" {
-        Err(StatusCode::NOT_FOUND)
-    } else {
-        let ttl = row.1 - timestamp_secs;
-        let set_cache: Result<String, redis::RedisError> = conn.set_ex(&token, &row.0,ttl as u64).await;
-        if set_cache.is_err() {
-            eprintln!("Failed to set token={}: {}", &token, set_cache.err().unwrap())
-
+    match state.redis.get_multiplexed_async_connection().await {
+        Ok(mut conn) => {
+            match conn.get(&token).await {
+                Result::<String, redis::RedisError>::Ok(r) => {
+                    log::info!("Responded from cache token={} to link={}", &token, &r);
+                    return Ok(Redirect::temporary(&r.as_str()));
+                },
+                Err(e) => {
+                    log::info!("No cached link for token={}", &token);
+                    drop(e);
+                    let now = SystemTime::now();
+                    let duration_since_epoch = now
+                        .duration_since(UNIX_EPOCH)
+                        .expect("Time went backwards");
+                    let timestamp_secs = duration_since_epoch.as_secs() as i32;
+                    match sqlx::query_as(FIND_REDIRECT).persistent(true).bind(&token).bind(timestamp_secs).fetch_one(&state.pool).await {
+                        Result::<(String , i32), _>::Ok(row) => {
+                            let ttl = row.1 - timestamp_secs;
+                            match  conn.set_ex(&token, &row.0,ttl as u64).await {
+                                Result::<String, redis::RedisError>::Ok(_) => {
+                                    log::info!("Cached link={} for token={}", &row.0, &token);
+                                },
+                                Err(e) => {
+                                    log::warn!("Failed to cache token={} for link={}: {}", &token,&row.0, e);
+                                    drop(e);
+                                }
+                            }
+                            log::info!("Responded token={} to link={}", &token, &row.0);
+                            return Ok(Redirect::temporary(&row.0))
+                        },
+                        Err(e) => {
+                            log::info!("Not found link for token={}: {}", &token, e);
+                            return Err(StatusCode::NOT_FOUND)
+                        }
+                    };
+                }
+            };
+        },
+        Err(e) => {
+            log::error!("Failed to get Redis connection for token={}: {}", &token, e);
+            drop(e);
+            let now = SystemTime::now();
+            let duration_since_epoch = now
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards");
+            let timestamp_secs = duration_since_epoch.as_secs() as i32;
+            match sqlx::query_as(FIND_REDIRECT).persistent(true).bind(&token).bind(timestamp_secs).fetch_one(&state.pool).await {
+                Result::<(String , i32), _>::Ok(row) => {
+                    log::info!("Responded token={} to link={}", &token, &row.0);
+                    return Ok(Redirect::temporary(&row.0))
+                },
+                Err(e) => {
+                    log::info!("Not found link for token={}: {}", &token, e);
+                    return Err(StatusCode::NOT_FOUND)
+                }
+            };
         }
-        Ok(Redirect::temporary(&row.0))
-    }
+    };
 }
